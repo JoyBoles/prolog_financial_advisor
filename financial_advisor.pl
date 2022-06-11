93976a1c4cb1249d8ea785414fa461bdb9d63b5b
JoyBoles <63476608+JoyBoles@users.noreply.github.com>
2022-06-11 22:43:56 +0200

    Update financial_advisor.pl

--- financial_advisor.pl
+++ financial_advisor.pl
@@ -4,12 +4,9 @@ investment(savings) :- savings(inadequate).
 investment(stocks) :- savings(adequate), income(adequate).
 investment(combo) :- savings(adequate), income(inadequate).
 savings(adequate) :- amount_saved(Amount),numDependents(Dependents),min_savings(Dependents,SavingsMin),Amount > SavingsMin.
-savings(adequate) :- numDependents(Dependents),Dependents == 0.
 savings(inadequate) :- amount_saved(Amount),numDependents(Dependents),min_savings(Dependents, SavingsMin), Amount =< SavingsMin.
 income(adequate) :- earnings(AmountEarned, steady), numDependents(Dependents),min_income(Dependents, IncomeMin), AmountEarned > IncomeMin.
 income(inadequate) :- earnings(AmountEarned, steady),numDependents(Dependents),min_income(Dependents, IncomeMin), AmountEarned =< IncomeMin.
-income(adequate) :- numDependents(Dependents), Dependents == 0.
-income(inadequate) :- earnings(AmountEarned, unsteady).
 income(inadequate) :- earnings(AmountEarned, unsteady).
 amount_saved(4000).
 numDependents(2).
